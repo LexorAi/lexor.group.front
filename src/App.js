817e85b571c757425c0eb308da38185da53b4d8d
João Teodozo <joaoteodozo@outlook.com>
2025-05-16 21:44:26 -0300

    atualizações

--- src/App.js
+++ src/App.js
@@ -16,30 +16,35 @@ function App() {
       {/* NAVBAR */}
       <nav className="navbar">
         <ul className="menu">
-          <li><a href="#inicio">Início</a></li>
-          <li><a href="#visao">Visão</a></li>
-          <li><a href="#missao">Missão</a></li>
-          <li><a href="#servicos">Serviços</a></li>
-          <li><a href="#produtos">Produtos</a></li>
-          <li><a href="#contato">Contato</a></li>
+          <li><a href="#inicio">INÍCIO</a></li>
+          {/* <li><a href="#visao">Visão</a></li>
+          <li><a href="#missao">Missão</a></li> */}
+          <li><a href="#servicos">SERVIÇOS</a></li>
+          <li><a href="#produtos">PRODUTOS</a></li>
+          <li><a href="#contato">CONTATO</a></li>
         </ul>
         <div className="logo">LEXOR</div>
       </nav>
       
-      <br></br><br></br><br></br>
+      <br></br><br></br>
       {/* CABEÇALHO PRINCIPAL */}
       <header id="inicio">
         <Header />
-        {/* <h1>Transformando Ideias em Soluções Digitais</h1>
-        <p className="slogan">Inovação, tecnologia e excelência para o seu negócio.</p>
-        <p className="descricao">
+        {/* <h1>Transformando Ideias em Soluções Digitais</h1>*/}
+        <p className="slogan">Impulsionamos os resultados dos nossos clientes por meio<br></br> 
+        de soluções digitais eficientes e entregas ágeis</p>
+        <br></br>
+        <button className="button">Contate-nos</button>
+        {/* da criação de soluções digitais e entregas ágeis */}
+        {/*<p className="descricao">
           Somos uma empresa de tecnologia voltada para o desenvolvimento de soluções inovadoras que impulsionam a transformação digital de negócios e melhoram a experiência das pessoas no uso da tecnologia. Com uma equipe multidisciplinar e apaixonada por inovação, atuamos na criação de produtos e serviços que combinam eficiência operacional, inteligência de dados, segurança e usabilidade. Desde a concepção de sistemas personalizados até a implementação de plataformas baseadas em inteligência artificial, automação e nuvem, nosso foco está em entregar valor real, acompanhando as necessidades do mercado e superando as expectativas de nossos clientes. Valorizamos a colaboração, a ética e o aprendizado contínuo, construindo parcerias sólidas e duradouras. Acreditamos que tecnologia é mais do que ferramenta — é o motor da evolução, e por isso colocamos o ser humano no centro de tudo o que desenvolvemos.
         </p> */}
       </header>
-
+          
+      <br></br><br></br><br></br><br></br>
       {/* CONTEÚDO PRINCIPAL */}
       <main>
-        <section id="visao" className="descricao" data-aos="fade-up">
+        {/* <section id="visao" className="descricao" data-aos="fade-up">
           <h2>Visão</h2>
           <p>
             Nossa visão é ser uma das principais referências em tecnologia e inovação no cenário nacional e internacional, reconhecida por nossa capacidade de antecipar tendências, entregar soluções que geram valor real e por impactar positivamente a sociedade...
@@ -51,7 +56,7 @@ function App() {
           <p>
             Nossa missão é criar e entregar soluções tecnológicas inteligentes, seguras e escaláveis, que ajudem empresas e pessoas a superarem desafios, otimizarem processos e explorarem novas possibilidades em um mundo cada vez mais digital...
           </p>
-        </section>
+        </section> */}
 
         <section id="servicos" className="descricao" data-aos="fade-up">
           <h2>Serviços</h2>
